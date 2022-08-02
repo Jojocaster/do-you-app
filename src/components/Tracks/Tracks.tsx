4fd6d175a9c1c525ed85d5e96ea80cec99eb7069
Joel Beaudon <joel.beaudon@gmail.com>
2022-08-02 11:30:50 +0100

    Tracks now uses setInterval if screen is focused

--- src/components/Tracks/Tracks.tsx
+++ src/components/Tracks/Tracks.tsx
@@ -17,47 +17,40 @@ export const Tracks: React.FC = () => {
   )
   const { status } = useSelector((state: RootState) => state.show)
 
-  const restartTimer = () => {
-    // always clear timeout to prevent leaks
-    if (timeout.current) {
-      clearTimeout(timeout.current)
-    }
+  // const restartTimer = () => {
+  //   // always clear timeout to prevent leaks
+  //   if (timeout.current) {
+  //     clearTimeout(timeout.current)
+  //   }
 
-    // only poll if show is live & screen is focused
-    if (status === ShowStatus.ON && isFocused) {
-      timeout.current = setTimeout(() => {
-        dispatch(fetchTracksInfo())
-      }, 1000 * 60)
-    }
-  }
+  //   // only poll if show is live & screen is focused
+  //   if (status === ShowStatus.ON && isFocused) {
+  //     timeout.current = setTimeout(() => {
+  //       dispatch(fetchTracksInfo())
+  //     }, 1000 * 60)
+  //   }
+  // }
 
-  const getTracks = async () => {
-    // if tracks were never fetched before, request them straight away
-    if (!lastUpdated) {
+  const loadTracks = () => {
+    if (status === ShowStatus.ON) {
       dispatch(fetchTracksInfo())
-    } else {
-      const now = new Date().getTime()
-      const diff = now - lastUpdated
-      const diffInMinutes = diff / (1000 * 60)
-      if (diffInMinutes >= 1) {
-        dispatch(fetchTracksInfo())
-      } else {
-        restartTimer()
-      }
     }
   }
 
   useEffect(() => {
     // fetch tracks when tab is focused
     if (isFocused) {
-      getTracks()
+      loadTracks()
+
+      timeout.current = setInterval(() => {
+        loadTracks()
+      }, 1000 * 60)
+    } else {
+      if (timeout.current) {
+        clearInterval(timeout.current)
+      }
     }
   }, [isFocused])
-
-  useEffect(() => {
-    // restart timer as soon as new tracks have been loaded
-    restartTimer()
-  }, [lastUpdated])
 
   if ((loading && !tracks.length) || (loading && !isToday(lastUpdated))) {
     return <Loader testID="loader">Loading bangers</Loader>
