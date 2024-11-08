44d5474c45d630f6884b059f0ece22e908073ef4
Joel Beaudon <joel.beaudon@gmail.com>
2024-11-08 12:18:21 +0000

    events disabled to fix crash

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -22,74 +22,75 @@ import { Text, View as CustomView } from '../Themed'
 
 export const Events = () => {
   const theme = useColorScheme()
-  const { events_data, event_dates } = useSelector(
-    (state: RootState) => state.events
-  )
+  // const { events_data, event_dates } = useSelector(
+  //   (state: RootState) => state.events
+  // )
   const layout = useWindowDimensions()
   const CustomTouchable =
     Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
 
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchEvents())
+    // dispatch(fetchEvents())
   }, [])
 
-  if (!event_dates?.length) {
-    return (
-      <View
-        style={{
-          height: layout.height / 2 - 75,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <MaterialCommunityIcons
-          color={Colors[theme].text}
-          name="radio-off"
-          size={20}
-        />
-        <Text style={{ marginTop: 10 }}>Nothing to see here - yet</Text>
-      </View>
-    )
-  }
+  // if (!event_dates?.length) {
 
   return (
-    <View style={{ height: layout.height / 2 - 75 }}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: Space.viewPadding }}
-      >
-        {events_data.map((event) => {
-          const date = new Date(event.start_date.date)
-          const formattedDate = format(date, 'E, MMM dd')
-          return (
-            //@ts-ignore
-            <CustomTouchable
-              key={event.id}
-              onPress={() => Linking.openURL(event.ticket_links[0].link)}
-            >
-              <CustomView style={styles.container}>
-                <Image style={styles.image} source={{ uri: event.image }} />
-                <View style={styles.content}>
-                  <Text
-                    style={{
-                      textTransform: 'uppercase',
-                      fontSize: 10,
-                      marginBottom: 5,
-                    }}
-                  >
-                    {formattedDate}
-                  </Text>
-                  <Text style={styles.title}>{event.title}</Text>
-                </View>
-              </CustomView>
-            </CustomTouchable>
-          )
-        })}
-      </ScrollView>
+    <View
+      style={{
+        height: layout.height / 2 - 75,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <MaterialCommunityIcons
+        color={Colors[theme].text}
+        name="radio-off"
+        size={20}
+      />
+      <Text style={{ marginTop: 10 }}>Nothing to see here - yet</Text>
     </View>
   )
+  // }
+
+  // return (
+  //   <View style={{ height: layout.height / 2 - 75 }}>
+  //     <ScrollView
+  //       showsVerticalScrollIndicator={false}
+  //       contentContainerStyle={{ paddingBottom: Space.viewPadding }}
+  //     >
+  //       {events_data.map((event) => {
+  //         const date = new Date(event.start_date.date)
+  //         const formattedDate = format(date, 'E, MMM dd')
+  //         return (
+  //           //@ts-ignore
+  //           <CustomTouchable
+  //             key={event.id}
+  //             onPress={() => Linking.openURL(event.ticket_links[0].link)}
+  //           >
+  //             <CustomView style={styles.container}>
+  //               <Image style={styles.image} source={{ uri: event.image }} />
+  //               <View style={styles.content}>
+  //                 <Text
+  //                   style={{
+  //                     textTransform: 'uppercase',
+  //                     fontSize: 10,
+  //                     marginBottom: 5,
+  //                   }}
+  //                 >
+  //                   {formattedDate}
+  //                 </Text>
+  //                 <Text style={styles.title}>{event.title}</Text>
+  //               </View>
+  //             </CustomView>
+  //           </CustomTouchable>
+  //         )
+  //       })}
+  //     </ScrollView>
+  //   </View>
+  // )
 }
 
 const styles = StyleSheet.create({
